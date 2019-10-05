c9e5942a0bd8ff998806bd73e8aa6eb6978c0121
Leo Farias <leo@conceptsol.com>
2019-10-04 23:48:44 -0400

    Cleaned go .dot files

---
+++ test/utils/flutter_tools_test.dart
@@ -0,0 +1,19 @@
+import 'package:test/test.dart';
+import 'package:fvm/utils/flutter_tools.dart';
+
+void main() {
+  test('gitClone', () async {
+    await flutterSdkClone('v1.8.4');
+    expect(true, true);
+  });
+
+  test('flutterSdkInfo', () async {
+    await flutterSdkInfo('master');
+    expect(true, true);
+  });
+
+  test('listSdkVersions', () async {
+    await listSdkVersions();
+    expect(true, true);
+  });
+}
