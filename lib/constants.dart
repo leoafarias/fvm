22aaa441d67ed5d33d5d7133dc60dc5641a89477
Leo Farias <leo@conceptsol.com>
2019-10-08 08:35:39 -0400

    Improved flutter_tools workfllow and changed utils and helpers

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,16 +7,26 @@ const kFlutterRepo = "https://github.com/flutter/flutter.git";
 /// Working Directory for FVM
 final kWorkingDirectory = Directory.current;
 
-// Removes the script from path using 'dirname'.
-final _fvmBinPath = dirname(Platform.script.path);
-// Remove /bin from path
-final _fvmRootPath = _fvmBinPath.substring(0, _fvmBinPath.length - 4);
+/// Local Project Flutter Link
+final kLocalFlutterLink = Link('${kWorkingDirectory.path}/fvm');
 
-/// Root directory for FVM
-final kFvmDir = Directory(_fvmRootPath);
-// TODO: Find better way to join paths
+/// FVM Home directory
+String _fvmHome() {
+  var home = "";
+  final envVars = Platform.environment;
+  if (Platform.isMacOS) {
+    home = envVars['HOME'];
+  } else if (Platform.isLinux) {
+    home = envVars['HOME'];
+  } else if (Platform.isWindows) {
+    home = envVars['UserProfile'];
+  }
+
+  return '$home/fvm';
+}
+
 /// Where Flutter SDK Versions are stored
-final kVersionsDir = Directory('${kFvmDir.path}/versions');
+final kVersionsDir = Directory('${_fvmHome()}/versions');
 
 /// Flutter Channels
 final kFlutterChannels = ['master', 'stable', 'dev', 'beta'];
--- lib/utils/flutter_tools.dart
+++ lib/utils/flutter_tools.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:fvm/constants.dart';
+import 'package:fvm/exceptions.dart';
 import 'package:fvm/utils/helpers.dart';
 import 'package:path/path.dart';
 import 'package:io/io.dart';
@@ -14,36 +15,80 @@ Future<void> processRunner(String cmd, List<String> args,
   await sharedStdIn.terminate();
 }
 
-/// Clones Flutter SDK from Channel
-Future<void> flutterChannelClone(String branch) async {
-  final branchDirectory = Directory('${kVersionsDir.path}/$branch');
-  await flutterSdkRemove(branch);
-  await branchDirectory.create(recursive: true);
+// TODO: force all branch and versions to be lowercase
 
-  await Process.run('git', ['clone', '-b', branch, kFlutterRepo, '.'],
-      workingDirectory: branchDirectory.path);
+/// Clones Flutter SDK from Channel
+/// Returns true if comes from exists or false if its new fetch.
+Future<void> flutterChannelClone(String channel) async {
+  final channelDirectory = Directory('${kVersionsDir.path}/$channel');
+
+  if (!isValidFlutterChannel(channel)) {
+    throw ExceptionNotValidChannel('"$channel" is not a valid channel');
+  }
+
+  if (await channelDirectory.exists()) {
+    return;
+  }
+
+  await channelDirectory.create(recursive: true);
+
+  var result = await Process.run(
+      'git', ['clone', '-b', channel, kFlutterRepo, '.'],
+      workingDirectory: channelDirectory.path);
+
+  if (result.exitCode != 0) {
+    throw ExceptionCouldNotClone("Could not clone $channel");
+  }
 }
 
 /// Clones Flutter SDK from Version Number
+/// Returns exists:true if comes from cache or false if its new fetch.
 Future<void> flutterVersionClone(String version) async {
   final versionDirectory = Directory('${kVersionsDir.path}/$version');
-  await flutterSdkRemove(version);
+
+  if (!await isValidFlutterVersion(version)) {
+    throw ExceptionNotValidVersion('"$version" is not a valid version');
+  }
+
+  if (await versionDirectory.exists()) {
+    return;
+  }
+
   await versionDirectory.create(recursive: true);
 
-  await Process.run('git', ['clone', '-b', 'v$version', kFlutterRepo, '.'],
+  var result = await Process.run(
+      'git', ['clone', '-b', 'v$version', kFlutterRepo, '.'],
       workingDirectory: versionDirectory.path);
+
+  if (result.exitCode != 0) {
+    throw ExceptionCouldNotClone("Could not clone $version");
+  }
 }
 
-/// Clones Flutter SDK
-Future<void> flutterSdkInfo(String branch) async {
-  final branchDirectory = Directory('${kVersionsDir.path}/$branch');
+/// Gets Flutter version from project
+// Future<String> flutterGetProjectVersion() async {
+//   final target = await kLocalFlutterLink.target();
+//   print(target);
+//   return await _gitGetVersion(target);
+// }
 
+/// Gets SDK Version
+Future<String> flutterSdkVersion(String branch) async {
+  final branchDirectory = Directory('${kVersionsDir.path}/$branch');
+  return await _gitGetVersion(branchDirectory.path);
+}
+
+Future<String> _gitGetVersion(String path) async {
   var result = await Process.run('git', ['rev-parse', '--abbrev-ref', 'HEAD'],
-      workingDirectory: branchDirectory.path);
+      workingDirectory: path);
 
   if (result.stdout.trim() == 'HEAD') {
     result = await Process.run('git', ['tag', '--points-at', 'HEAD'],
-        workingDirectory: branchDirectory.path);
+        workingDirectory: path);
+  }
+
+  if (result.exitCode != 0) {
+    throw Exception('Could not get version Info.');
   }
 
   final versionNumber = result.stdout.trim();
@@ -54,7 +99,11 @@ Future<void> flutterSdkInfo(String branch) async {
 Future<List<String>> flutterListAllSdks() async {
   final result =
       await Process.run('git', ['ls-remote', '--tags', '$kFlutterRepo']);
-// refs/tags/
+
+  if (result.exitCode != 0) {
+    throw Exception('Could not fetch list of available Flutter SDKs');
+  }
+
   List<String> tags = result.stdout.split('\n');
 
   var versionsList = <String>[];
@@ -70,10 +119,10 @@ Future<List<String>> flutterListAllSdks() async {
 }
 
 /// Removes a Version of Flutter SDK
-Future<void> flutterSdkRemove(String branch) async {
-  final branchDirectory = Directory('${kVersionsDir.path}/$branch');
-  if (await branchDirectory.exists()) {
-    await branchDirectory.delete(recursive: true);
+Future<void> flutterSdkRemove(String version) async {
+  final versionDir = Directory('${kVersionsDir.path}/$version');
+  if (await versionDir.exists()) {
+    await versionDir.delete(recursive: true);
   }
 }
 
@@ -93,19 +142,8 @@ Future<List<String>> flutterListInstalledSdks() async {
   return results;
 }
 
-/// Create Flutter Dir within the project
-Future<void> createProjectFlutterDir() async {
-  final flutterDir = Directory('${kWorkingDirectory.path}/.flutter');
-  if (await flutterDir.exists()) {
-    await flutterDir.delete(recursive: true);
-  }
-  await flutterDir.create(recursive: true);
-}
-
-/// Links Flutter Dir to cached SDK
+/// Links Flutter Dir to existsd SDK
 Future<void> linkProjectFlutterDir(String version) async {
   final versionBin = Directory('${kVersionsDir.path}/$version/bin/flutter');
-  final flutterDir = Directory('${kWorkingDirectory.path}/flutter');
-  await createProjectFlutterDir();
-  await linkDir(flutterDir, versionBin);
+  await linkDir(kLocalFlutterLink, versionBin);
 }
