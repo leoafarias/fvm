ba64552ef988a84ffbe819977f2348ef011f2869
leoafarias <leokcta@gmail.com>
2020-06-19 07:14:37 -0400

    Linter issues and test clean-up changes

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -3,20 +3,21 @@ import 'dart:io';
 import 'package:fvm/constants.dart';
 import 'package:fvm/utils/config_utils.dart';
 
-void fvmTearDownAll() async {
+void cleanup() async {
   final fvmHomeDir = Directory(fvmHome);
   if (await fvmHomeDir.exists()) {
     await fvmHomeDir.delete(recursive: true);
   }
-  await ConfigUtils().removeConfig();
+  ConfigUtils().removeConfig();
+  kProjectFvmDir.deleteSync(recursive: true);
+}
+
+void fvmTearDownAll() async {
+  cleanup();
 }
 
 void fvmSetUpAll() async {
   // Looks just like Teardown rightnow bu
   // will probalby change. Just to guarantee a clean run
-  final fvmHomeDir = Directory(fvmHome);
-  if (await fvmHomeDir.exists()) {
-    await fvmHomeDir.delete(recursive: true);
-  }
-  await ConfigUtils().removeConfig();
+  cleanup();
 }
