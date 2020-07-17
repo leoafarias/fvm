a5667acaee95c7fc5ec993a0325ddd66d764ea84
leoafarias <leokcta@gmail.com>
2020-07-17 13:15:47 -0400

    update

--- lib/commands/flutter.dart
+++ lib/commands/flutter.dart
@@ -1,7 +1,6 @@
 import 'package:args/command_runner.dart';
 import 'package:fvm/constants.dart';
 import 'package:fvm/utils/flutter_tools.dart';
-import 'package:fvm/utils/guards.dart';
 import 'package:fvm/utils/helpers.dart';
 import 'package:args/args.dart';
 
