9ac9822b6076ae222a0bd7a89f752716ae380444
leoafarias <leoafarias@gmail.com>
2020-09-24 11:20:15 -0700

    Windows installer script

---
+++ packages/app/lib/setup.bat
@@ -0,0 +1,9 @@
+vcredist_x86.exe
+mkdir "C:\Program Files\fvm"
+copy fvm.exe "C:\Program Files\fvm"
+copy data "C:\Program Files\fvm"
+copy file_chooser_plugin.dll "C:\Program Files\fvm"
+copy flutter_windows.dll "C:\Program Files\fvm"
+copy path_provider_fde_plugin.dll "C:\Program Files\fvm"
+copy url_launcher_windows_plugin.dll "C:\Program Files\fvm"
+copy window_size_plugin "C:\Program Files\fvm"
