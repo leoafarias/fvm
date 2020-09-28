f2af655a52c7c617b6151eeaed3d5243a282216d
leoafarias <leokcta@gmail.com>
2020-09-28 10:24:28 -0400

    Grind tasks

---
+++ packages/cli/tool/grind.dart
@@ -0,0 +1,13 @@
+import 'package:grinder/grinder.dart';
+import 'package:cli_pkg/cli_pkg.dart' as pkg;
+import 'package:dotenv/dotenv.dart' show env;
+
+void main(List<String> args) {
+  pkg.name = 'fvm';
+  pkg.humanName = 'fvm';
+  pkg.githubUser = env['GITHUB_USERNAME'];
+  pkg.githubPassword = env['GITHUB_PASSWORD'];
+
+  pkg.addAllTasks();
+  grind(args);
+}
