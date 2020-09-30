1d5b6914470f1421d89b1b2297e3a4785c8d6992
leoafarias <leokcta@gmail.com>
2020-09-30 10:50:59 -0400

    Clean up and workflows

--- packages/cli/tool/grind.dart
+++ packages/cli/tool/grind.dart
@@ -5,8 +5,8 @@ import 'package:dotenv/dotenv.dart' show env;
 void main(List<String> args) {
   pkg.name = 'fvm';
   pkg.humanName = 'fvm';
-  pkg.githubUser = env['GITHUB_USERNAME'];
-  pkg.githubPassword = env['GITHUB_PASSWORD'];
+  pkg.githubUser = 'leoafarias';
+  pkg.githubPassword = env['GITHUB_TOKEN'];
   pkg.homebrewRepo = 'leoafarias/homebrew-fvm';
 
   pkg.addAllTasks();
