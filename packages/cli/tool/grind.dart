f92f4d71650d0644549707ad7444a4de6004adce
leoafarias <leokcta@gmail.com>
2020-09-30 11:30:22 -0400

    Removed githubtoken_env

--- packages/cli/tool/grind.dart
+++ packages/cli/tool/grind.dart
@@ -1,12 +1,10 @@
 import 'package:grinder/grinder.dart';
 import 'package:cli_pkg/cli_pkg.dart' as pkg;
-import 'package:dotenv/dotenv.dart' show env;
 
 void main(List<String> args) {
   pkg.name = 'fvm';
   pkg.humanName = 'fvm';
   pkg.githubUser = 'leoafarias';
-  pkg.githubPassword = env['GITHUB_TOKEN'];
   pkg.homebrewRepo = 'leoafarias/homebrew-fvm';
 
   pkg.addAllTasks();
