573b957eb51bf8a7182fa08801b9c594459fd310
leoafarias <leokcta@gmail.com>
2020-10-08 14:40:19 -0400

    Update grind

--- packages/cli/tool/grind.dart
+++ packages/cli/tool/grind.dart
@@ -6,7 +6,6 @@ void main(List<String> args) {
   pkg.humanName = 'fvm';
   pkg.githubUser = 'leoafarias';
   pkg.homebrewRepo = 'leoafarias/homebrew-fvm';
-  pkg.homebrewTag = 'v${pkg.pubspec.version.toString()}';
   pkg.addAllTasks();
   grind(args);
 }
