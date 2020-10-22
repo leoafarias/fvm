baf0e24870f938494f9fcd7b0eb11ba415c4dd79
leoafarias <leokcta@gmail.com>
2020-10-22 18:13:28 -0400

    Further improvements on package screen

--- packages/app/lib/providers/project_dependencies.provider.dart
+++ packages/app/lib/providers/project_dependencies.provider.dart
@@ -6,7 +6,7 @@ import 'package:hooks_riverpod/all.dart';
 // ignore: top_level_function_literal_block
 final projectDependenciesProvider = FutureProvider((ref) {
   final projects = ref.watch(projectsProvider.state);
-  final packages = <String>{};
+  final packages = <String, int>{};
 
   for (var project in projects.list) {
     final pubspec = project.pubspec;
@@ -19,7 +19,7 @@ final projectDependenciesProvider = FutureProvider((ref) {
     allDeps.forEach((dep) {
       // ignore: invalid_use_of_protected_member
       if (dep.hosted != null && !isGooglePubPackage(dep.package())) {
-        packages.add(dep.package());
+        packages.update(dep.package(), (val) => ++val, ifAbsent: () => 0);
       } else {
         print(dep.package());
       }
--- packages/app/lib/utils/dependencies.dart
+++ packages/app/lib/utils/dependencies.dart
@@ -8,38 +8,49 @@ class PackageDetail {
   final PubPackage package;
   @required
   final PackageScore score;
-  PackageDetail({this.package, this.score});
+  @required
+  final int count;
+  PackageDetail({
+    this.package,
+    this.score,
+    this.count,
+  });
 }
 
 Map<String, PubPackage> mapPackages;
 
 /// Fetches all packages info from pub.dev
-Future<List<PackageDetail>> fetchAllDependencies(Set<String> packages) async {
+Future<List<PackageDetail>> fetchAllDependencies(
+    Map<String, int> packages) async {
   final pkgFutures = <Future<PubPackage>>[];
 
-  for (var pkg in packages) {
+  for (var pkg in packages.keys) {
     pkgFutures.add(client.packageInfo(pkg));
   }
   final packagesRes = await Future.wait(pkgFutures);
 
   final validPubPkgs = packagesRes.where((dep) => dep.name != null).toList();
 
-  return fetchAllScores(validPubPkgs);
+  return fetchAllScores(validPubPkgs, packages);
 }
 
-Future<List<PackageDetail>> fetchAllScores(List<PubPackage> packages) async {
+Future<List<PackageDetail>> fetchAllScores(
+  List<PubPackage> validPubPkgs,
+  Map<String, int> packages,
+) async {
   final pkgs = <Future<PackageDetail>>[];
 
-  Future<PackageDetail> _assignScore(PubPackage package) async {
+  Future<PackageDetail> _assignScore(PubPackage package, int count) async {
     final score = await client.packageScore(package.name);
     return PackageDetail(
       score: score,
       package: package,
+      count: count,
     );
   }
 
-  for (var pkg in packages) {
-    pkgs.add(_assignScore(pkg));
+  for (var pkg in validPubPkgs) {
+    pkgs.add(_assignScore(pkg, packages[pkg.name]));
   }
 
   return await Future.wait(pkgs);
