3d63cd44f2d6bc30041662d7ed4922fcee7e2fb1
leoafarias <leokcta@gmail.com>
2021-02-18 17:18:53 -0300

    Removed packages directory

--- packages/cli/lib/src/utils/process_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io' as io;
-
-import 'package:fvm/src/utils/logger.dart';
-import 'package:io/io.dart';
-
-final processManager = ProcessManager(
-  stderr: io.IOSink(consoleController.stderrSink),
-  stdout: io.IOSink(consoleController.stdoutSink),
-);
--- packages/cli/test/test_cases.md
+++
@@ -1,6 +0,0 @@
-# Test Cases
-
-- Run flutter proxy and no flutter version is configured
-- Install workflow with channel
-- Install workflow with version
-- Install workflow without channel or version
