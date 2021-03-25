c36f7394fca99c3bb88ba57af30f8ba02cceba20
leoafarias <leokcta@gmail.com>
2021-03-25 19:47:00 -0400

    Dependency issue

--- lib/src/commands/use_command.dart
+++ lib/src/commands/use_command.dart
@@ -1,10 +1,9 @@
 import 'package:args/command_runner.dart';
-import 'package:cli_dialog/cli_dialog.dart';
 
 import 'package:fvm/constants.dart';
-import 'package:fvm/fvm.dart';
+
 import 'package:fvm/src/flutter_tools/flutter_helpers.dart';
-import 'package:fvm/src/local_versions/local_version.repo.dart';
+
 import 'package:fvm/src/utils/pubdev.dart';
 
 import 'package:fvm/src/workflows/use_version.workflow.dart';
@@ -37,24 +36,10 @@ class UseCommand extends Command<int> {
   Future<int> run() async {
     String version;
 
-    // If no version is provider show selection
+    // If no version return error
+    //TODO: Provide version selection
     if (argResults.rest.isEmpty) {
-      final installedSdks = await LocalVersionRepo.getAll();
-      if (installedSdks.isEmpty) {
-        throw Exception('Please install a version. fvm install <version>');
-      }
-      final listQuestions = [
-        [
-          {
-            'question': 'Select version',
-            'options': installedSdks.map((e) => e.name).toList(),
-          },
-          'version'
-        ]
-      ];
-      final dialog = CLI_Dialog(listQuestions: listQuestions);
-      final answer = dialog.ask();
-      version = answer['version'] as String;
+      throw Exception('Please provide a version to use');
     }
 
     version ??= argResults.rest[0];
