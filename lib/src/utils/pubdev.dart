facae33cc06adfdfc18db773d2489a2971753b7d
leoafarias <leokcta@gmail.com>
2021-04-13 07:58:11 -0400

    Removed pub_api_client dep

--- lib/src/utils/pubdev.dart
+++ lib/src/utils/pubdev.dart
@@ -1,25 +1,44 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:io/ansi.dart';
-import 'package:pub_api_client/pub_api_client.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 import '../version.dart';
+
+const _packageUrl = 'https://pub.dev/api/packages/fvm';
+const _changelogUrl = 'https://pub.dev/packages/fvm/changelog';
+
+Future<String> _fetchLatestVersion() async {
+  final response = await http.get(_packageUrl);
+  final json = jsonDecode(response.body) as Map<String, dynamic>;
+  final version = json['latest']['version'] as String;
+  return version;
+}
 
 /// Checks if there is an update for [currentVersion]
 /// If not provided defaults to [packageVersion]
 Future<bool> checkIfLatestVersion({String currentVersion}) async {
   currentVersion ??= packageVersion;
   try {
-    final client = PubClient();
+    final latestVersion = await _fetchLatestVersion();
 
-    final latest =
-        await client.checkLatest('fvm', currentVersion: currentVersion);
+    final latestSemVer = Version.parse(latestVersion);
+    var needUpdate = false;
+    if (currentVersion != null) {
+      final current = Version.parse(currentVersion);
+      // Check as need update if latest version is higher
+      needUpdate = latestSemVer > current;
+    }
 
-    if (latest.needUpdate) {
+    if (needUpdate) {
       final updateCmd = cyan.wrap('pub global activate fvm');
 
       print(_divider);
       print(
-          '''FVM Update Available $packageVersion → ${green.wrap(latest.latestVersion)} ''');
-      print('${yellow.wrap('Changelog:')} ${latest.packageInfo.changelogUrl}');
+        '''FVM Update Available $packageVersion → ${green.wrap(latestVersion)} ''',
+      );
+      print('${yellow.wrap('Changelog:')} $_changelogUrl');
       print('Run $updateCmd to update');
       print(_divider);
       return false;
@@ -32,6 +51,7 @@ Future<bool> checkIfLatestVersion({String currentVersion}) async {
 }
 
 String get _divider {
-  return yellow
-      .wrap('\n___________________________________________________\n\n');
+  return yellow.wrap(
+    '\n___________________________________________________\n\n',
+  );
 }
