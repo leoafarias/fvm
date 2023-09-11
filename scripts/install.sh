fc6bfcdba88f8822fc95aa5c03674da05ef839d8
Leo Farias <leoafarias@gmail.com>
2023-09-10 22:56:52 -0400

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Custom Trap for runtime errors and Exit
+trap 'catchErrors $? $LINENO' EXIT ERR
+
+catchErrors() {
+    local ERR_CODE=$1
+    local ERR_LINE=$2
+    if [[ "$ERR_CODE" -ne 0 ]]; then
+        error "An error occurred on line $ERR_LINE of the script"
+    fi
+    rm -f fvm.tar.gz > /dev/null 2>&1
+}
+
+
+# Terminal colors setup
+Color_Off=''    # Reset
+Red=''          # Red
+Green=''        # Green
+Dim=''          # White
+Bold_White=''   # Bold White
+Bold_Green=''   # Bold Green
+
+if [[ -t 1 ]]; then
+    Color_Off='\033[0m'     # Reset
+    Red='\033[0;31m'        # Red
+    Green='\033[0;32m'      # Green
+    Dim='\033[0;2m'         # White
+    Bold_White='\033[1m'    # Bold White
+    Bold_Green='\033[1;32m' # Bold Green
+fi
+
+# Custom output functions
+error() {
+    printf "${Red}error${Color_Off}:" "$@" >&2
+    exit 1
+}
+
+info() {
+    echo -e "${Dim}$@ ${Color_Off}"
+}
+
+info_bold() {
+    echo -e "${Bold_White}$@ ${Color_Off}"
+}
+
+success() {
+    echo -e "${Green}$@ ${Color_Off}"
+}
 
 # Detect OS and architecture
 OS="$(uname -s)"
@@ -19,69 +68,62 @@ case "$ARCH" in
 esac
 
 # Log detected OS and architecture
-echo "Detected OS: $OS"
-echo "Detected Architecture: $ARCH"
+info "Detected OS: $OS"
+info "Detected Architecture: $ARCH"
 
 # Check for curl
 if ! command -v curl &> /dev/null; then
-    echo "curl is required but not installed. Exiting."
-    exit 1
+    error "curl is required but not installed"
 fi
 
-# Get installed FVM version if exists
-INSTALLED_FVM_VERSION=""
-if command -v fvm &> /dev/null; then
-    INSTALLED_FVM_VERSION=$(fvm --version)
-fi
+github_repo="fluttertools/fvm"
 
 # Define the URL of the FVM binary
-if [ -z "$1" ]; then
-  FVM_VERSION=$(curl -s https://api.github.com/repos/fluttertools/fvm/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-  if [ -z "$FVM_VERSION" ]; then
-      echo "Failed to fetch latest FVM version. Exiting."
-      exit 1
-  fi
+if [ -z "${1:-}" ]; then
+    FVM_VERSION=$(curl -s https://api.github.com/repos/$github_repo/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    if [ -z "$FVM_VERSION" ]; then
+        error "Failed to fetch the latest FVM version from GitHub"
+    fi
 else
   FVM_VERSION=$1
 fi
 
-# Compare versions and ask for user input if needed
-if [[ "$INSTALLED_FVM_VERSION" == "$FVM_VERSION" ]]; then
-     read -p "FVM version $FVM_VERSION is already installed. Would you like to reinstall it? (y/n): " REINSTALL
-    if [[ "$REINSTALL" != "y" ]]; then exit 0; fi
-elif [[ "$INSTALLED_FVM_VERSION" < "$FVM_VERSION" ]]; then
-    read -p "A newer FVM version ($FVM_VERSION) is available. Would you like to upgrade from $INSTALLED_FVM_VERSION? (y/n): " UPGRADE
-    if [[ "$UPGRADE" != "y" ]]; then exit 0; fi
-fi
 # Log version
-echo "Installing FVM Version: $FVM_VERSION"
+info "Installing FVM Version: $FVM_VERSION"
 
 # Download FVM
-URL="https://github.com/fluttertools/fvm/releases/download/$FVM_VERSION/fvm-$FVM_VERSION-$OS-$ARCH.tar.gz"
-curl -L $URL -o fvm.tar.gz
-if [ $? -ne 0 ]; then
-    echo "Download failed. Check your internet."
-    exit 1
-fi
+URL="https://github.com/$github_repo/releases/download/$FVM_VERSION/fvm-$FVM_VERSION-$OS-$ARCH.tar.gz"
+
+
+curl --fail --location --progress-bar --output "fvm.tar.gz" "$URL" ||
+    error "Failed to download FVM from \"$URL\""
 
 # Binary directory
 FVM_DIR="/usr/local/bin"
 
 # Extract binary
-tar xzf fvm.tar.gz -C $FVM_DIR
-if [ $? -ne 0 ]; then
-    echo "Extraction failed. Exiting."
-    exit 1
+if ! tar xzf fvm.tar.gz -C "$FVM_DIR"; then
+    error "Extraction failed"
 fi
+
+# Set permission
+chmod +x "$FVM_DIR/fvm" || error "Failed setting permissions"
 
 # Cleanup
-rm -f fvm.tar.gz
-
-# Verify installation
-if ! command -v fvm &> /dev/null; then
-    echo "Installation failed. Exiting."
-    exit 1
+if ! rm -f fvm.tar.gz; then
+    error "Failed to cleanup"
 fi
 
-INSTALLED_FVM_VERSION=$(fvm --version)
-echo "FVM $INSTALLED_FVM_VERSION installed successfully."
+
+
+if [[ "$(fvm --version)" != "$FVM_VERSION" ]]; then
+    error "FVM version verification failed."
+fi
+
+# Verify installation
+if command -v fvm &> /dev/null; then
+    INSTALLED_FVM_VERSION=$(fvm --version)
+    success "FVM $INSTALLED_FVM_VERSION installed successfully."
+else
+    error "Installation failed. Exiting."
+fi
