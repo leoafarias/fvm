b66e7e1e53b4540e6da27fc75c203644e543a27e
Nidal Bakir <nidal.bakir.edu@gmail.com>
2024-06-07 15:30:59 +0300

    feat: add the fvm bin path to PATH var automatically feat: introduce a update script to avoid adding fvm bin path to PATH var every time the user run install.sh fix:  remove the check to see if fvm is installed

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,20 +5,17 @@ OS="$(uname -s)"
 
 # Map to FVM naming
 case "$OS" in
-  Linux*)  OS='linux' ;;
-  Darwin*) OS='macos' ;;
-  *)       log_message "Unsupported OS"; exit 1 ;;
+Linux*) OS='linux' ;;
+Darwin*) OS='macos' ;;
+*)
+    log_message "Unsupported OS"
+    exit 1
+    ;;
 esac
 
 # Define the FVM directory and binary path
 FVM_DIR="$HOME/.fvm_flutter"
-
-# Check if FVM is installed
-if ! command -v fvm &> /dev/null
-then
-    echo "FVM is not installed. Exiting."
-    exit 1
-fi
+FVM_DIR_BIN="$FVM_DIR/bin"
 
 # Remove the FVM binary
 echo "Uninstalling FVM..."
@@ -27,11 +24,10 @@ rm -rf "$FVM_DIR" || {
     exit 1
 }
 
-echo "You can remove \"export PATH=$FMV_DIR_BIN:\$PATH\" from your ~/.bashrc, ~/.zshrc (or similar)"
+echo "You can remove \"export PATH=$FVM_DIR_BIN:\$PATH\" from your ~/.bashrc, ~/.zshrc (or similar)"
 
 # Check if uninstallation was successful
-if command -v fvm &> /dev/null
-then
+if command -v fvm &>/dev/null; then
     echo "Uninstallation failed. Please try again later."
     exit 1
 fi
