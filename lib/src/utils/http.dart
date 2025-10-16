5a34d5f927f9b01f84bb143a4ee756b0a99eb9ac
Leo Farias <leoafarias@gmail.com>
2025-10-16 12:42:50 -0400

    Improve error handling and file locking (#939)

--- lib/src/utils/http.dart
+++ lib/src/utils/http.dart
@@ -5,29 +5,31 @@ import 'dart:io';
 Future<String> httpRequest(String url, {Map<String, String>? headers}) async {
   final client = HttpClient();
 
-  // Add headers to request
+  try {
+    final request = await client.getUrl(Uri.parse(url));
 
-  final request = await client.getUrl(Uri.parse(url));
+    if (headers != null) {
+      headers.forEach((key, value) {
+        request.headers.set(key, value);
+      });
+    }
 
-  // Add headers to request
-  if (headers != null) {
-    headers.forEach((key, value) {
-      request.headers.set(key, value);
-    });
+    final response = await request.close();
+
+    if (response.statusCode >= 400) {
+      throw HttpException(
+        'HTTP ${response.statusCode}: ${response.reasonPhrase} - URL: $url',
+      );
+    }
+
+    final stream = response.transform(const Utf8Decoder());
+    final buffer = StringBuffer();
+    await for (final data in stream) {
+      buffer.write(data);
+    }
+
+    return buffer.toString();
+  } finally {
+    client.close();
   }
-
-  final response = await request.close();
-
-  if (response.statusCode >= 400) {
-    throw HttpException(response.reasonPhrase);
-  }
-
-  final stream = response.transform(Utf8Decoder());
-
-  var res = '';
-  await for (final data in stream) {
-    res += data;
-  }
-
-  return res;
 }
